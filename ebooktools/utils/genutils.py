6c9d321aa081a4d6de2d770c224c280417cf92de
Raul <rchfe23@gmail.com>
2021-03-15 20:49:26 -0400

    genutils: add run_cmd()

--- ebooktools/utils/genutils.py
+++ ebooktools/utils/genutils.py
@@ -2,6 +2,9 @@
 """
 import logging.config
 import os
+import shlex
+import subprocess
+import sys
 from logging import NullHandler
 
 
@@ -42,3 +45,45 @@ def init_log(module__name__, module___file__=None, package_name=None):
         logger_ = logging.getLogger(module__name__)
     logger_.addHandler(NullHandler())
     return logger_
+
+
+def run_cmd(cmd):
+    """Run a shell command with arguments.
+
+    The shell command is given as a string but the function will split it in
+    order to get a list having the name of the command and its arguments as
+    items.
+
+    Parameters
+    ----------
+    cmd : str
+        Command to be executed, e.g. ::
+
+            open -a TextEdit text.txt
+
+    Returns
+    -------
+    retcode: int
+        Returns code which is 0 if the command was successfully completed.
+        Otherwise, the return code is non-zero.
+
+    Raises
+    ------
+    FileNotFoundError
+        Raised if the command ``cmd`` is not recognized, e.g.
+        ``$ TextEdit {filepath}`` since `TextEdit` is not an executable.
+
+    """
+    try:
+        if sys.version_info.major == 3 and sys.version_info.minor <= 6:
+            # TODO: PIPE not working as arguments and capture_output new in
+            # Python 3.7
+            # Ref.: https://stackoverflow.com/a/53209196
+            #       https://bit.ly/3lvdGlG
+            result = subprocess.run(shlex.split(cmd))
+        else:
+            result = subprocess.run(shlex.split(cmd), capture_output=True)
+    except FileNotFoundError:
+        raise
+    else:
+        return result
