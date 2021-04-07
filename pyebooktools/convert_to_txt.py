51fa6ef8e88f65978cff84bb5540c37352b7352d
Raul <rchfe23@gmail.com>
2021-04-07 17:09:40 -0400

    convert_to_txt.py: update description

--- pyebooktools/convert_to_txt.py
+++ pyebooktools/convert_to_txt.py
@@ -2,8 +2,8 @@
 
 It can optionally also use *OCR* for `.pdf`, `.djvu` and image files.
 
-This is a Python port of `convert-to-txt.sh`_ from `ebook-tools`_ written
-in Shell by `na--`_.
+This is a Python port of `convert-to-txt.sh`_ from `ebook-tools`_ written in
+Shell by `na--`_.
 
 References
 ----------
@@ -12,7 +12,7 @@ References
 .. URLs
 
 .. external links
+.. _convert-to-txt.sh: https://github.com/na--/ebook-tools/blob/master/convert-to-txt.sh
 .. _ebook-tools: https://github.com/na--/ebook-tools
 .. _na--: https://github.com/na--
-.. _convert-to-txt.sh: https://github.com/na--/ebook-tools/blob/master/convert-to-txt.sh
 """
