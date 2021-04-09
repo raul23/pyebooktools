e44e4eb7119b4749ea84c2dfe80b8e6364ebcc38
Raul <rchfe23@gmail.com>
2021-04-09 11:05:19 -0400

    convert_to_txt.convert(): call lib.convert_to_txt()

--- py_ebooktools/convert_to_txt.py
+++ py_ebooktools/convert_to_txt.py
@@ -16,30 +16,60 @@ References
 .. _ebook-tools: https://github.com/na--/ebook-tools
 .. _na--: https://github.com/na--
 """
+import os
 from pathlib import Path
 
-import ipdb
-
 from py_ebooktools.configs import default_config as default_cfg
+from py_ebooktools.lib import convert_to_txt, get_mime_type, isalnum_in_file
 from py_ebooktools.utils.genutils import init_log, touch
 
 logger = init_log(__name__, __file__)
 
 
-def convert(input_file, output_file=default_cfg.output_file, **kwargs):
+def convert(input_file, output_file=default_cfg.output_file,
+            ocr_enabled=default_cfg.ocr_enabled, **kwargs):
+    # TODO: Path(input_file)
+    # TODO: check that input_file exists
+    output_file = Path(output_file)
+    import ipdb
+    mime_type = get_mime_type(input_file)
     # Check first that the output text file is valid
-    if Path(output_file).suffix != '.txt':
+    if output_file.suffix != '.txt':
         # TODO: ? raise error and catch exception in ebooktools
-        logger.error('The output file needs to have a .txt extension!')
+        logger.error("The output file needs to have a .txt extension!")
         return 1
     # Create output file text if it doesn't exist
-    ipdb.set_trace()
-    if Path(output_file).exists():
-        logger.info(f"Output text file already exists: {output_file}")
+    if output_file.exists():
+        logger.info(f"Output text file already exists: {output_file.name}")
         logger.debug("Full path of output text file: "
-                     f"{Path(output_file).absolute()}")
+                     f"{output_file.absolute()}")
     else:
         # Create output text file
+        # TODO: as tmp file?
         touch(output_file)
-    ipdb.set_trace()
+    if ocr_enabled == 'always':
+        logger.debug("OCR=always, first try OCR then conversion")
+        # TODO: call lib.ocr_file() and then convert_to_txt()
+        print("ocr_file()")
+        print("convert_to_txt()")
+    elif ocr_enabled == 'true':
+        logger.debug("OCR=true, first try conversion and then OCR")
+        # Check if valid converted text file
+        result = convert_to_txt(input_file, output_file, mime_type)
+        ipdb.set_trace()
+        if result.returncode == 0 and isalnum_in_file(output_file):
+            logger.debug("conversion successful, will not try OCR")
+        else:
+            # TODO: call lib.ocr_file()
+            print("ocr_file()")
+    else:
+        logger.debug("OCR=false, try only conversion")
+        result = convert_to_txt(input_file, output_file, mime_type)
+        if result.returncode == 0 and isalnum_in_file(output_file):
+            logger.debug("conversion successful")
+        else:
+            logger.warning('The converted txt with size '
+                           f'{os.stat(output_file).st_size} bytes does not'
+                           'seem to contain text')
+    # TODO: ? remove tmp output text file
     return 0
