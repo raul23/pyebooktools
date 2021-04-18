0ff443125bdae61a613f8063346ddc49067c4a14
Raul <rchfe23@gmail.com>
2021-04-18 17:45:29 -0400

    rename_calibre_library: add description

---
+++ py_ebooktools/rename_calibre_library.py
@@ -0,0 +1,18 @@
+"""Traverses a calibre library folder and renames all the book files in it by
+reading their metadata from calibre's metadata.opf files.
+
+This is a Python port of `rename-calibre-library.sh`_ from `ebook-tools`_
+written in Shell by `na--`_.
+
+References
+----------
+* `ebook-tools`_
+
+.. TODO: add description to reference (and other places too)
+.. URLs
+
+.. external links
+.. _ebook-tools: https://github.com/na--/ebook-tools
+.. _na--: https://github.com/na--
+.. _rename-calibre-library.sh: https://github.com/na--/ebook-tools/blob/master/rename-calibre-library.sh
+"""
