976d0ef8128b87443d518fa35d7d2daa61a41b51
Raul <rchfe23@gmail.com>
2021-04-18 17:46:28 -0400

    Update

--- py_ebooktools/split_into_folders.py
+++ py_ebooktools/split_into_folders.py
@@ -1,4 +1,4 @@
-"""Split the supplied ebook files (and the accompanying metadata files if
+"""Splits the supplied ebook files (and the accompanying metadata files if
 present) into folders with consecutive names that each contain the specified
 number of files.
 
