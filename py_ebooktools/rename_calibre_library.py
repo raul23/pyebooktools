dd6c630f2e0cbbca2252391e420eb2c7d346f57c
Raul <rchfe23@gmail.com>
2021-04-18 23:21:35 -0400

    rename: find ebooks with corresponding metadata

--- py_ebooktools/rename_calibre_library.py
+++ py_ebooktools/rename_calibre_library.py
@@ -16,6 +16,8 @@ References
 .. _na--: https://github.com/na--
 .. _rename-calibre-library.sh: https://github.com/na--/ebook-tools/blob/master/rename-calibre-library.sh
 """
+from pathlib import Path
+
 from py_ebooktools.configs import default_config as default_cfg
 from py_ebooktools.utils.genutils import init_log
 
@@ -24,5 +26,34 @@ logger = init_log(__name__, __file__)
 
 def rename(calibre_folder, output_folder=default_cfg.output_folder,
            dry_run=default_cfg.dry_run, reverse=default_cfg.file_sort_reverse,
-           **kwargs):
-    logger.info('test')
+           save_metadata=default_cfg.save_metadata,
+           symlink_only=default_cfg.symlink_only, **kwargs):
+    # TODO: remove
+    import ipdb
+    number_ebooks = 0
+    for book_path in Path(calibre_folder).rglob('*'):
+        logger.debug(f'Processing {book_path}')
+        if book_path.is_dir():
+            logger.debug('Rejected! It is a folder')
+            continue
+        elif not book_path.suffix:
+            logger.debug('Rejected! It is a hidden file')
+            continue
+        elif book_path.suffix == '.opf':
+            logger.debug('Rejected! It is a metadata file')
+            continue
+        elif book_path.name == 'cover.jpg':
+            logger.debug('Rejected! It is the cover image of the ebook')
+            continue
+        else:
+            logger.debug('We found a book file!')
+        logger.debug('Checking if the book file has a metadata file associated with')
+        metadata_path = book_path.parent.joinpath('metadata.opf')
+        if not metadata_path.exists():
+            logger.warning(f"Skipping file '{book_path.name}' - no metadata.opf present!")
+            logger.debug(f'Full path: {book_path}')
+        number_ebooks += 1
+        logger.info(f"Found file '{book_path.name}' with metadata.opf present, parsing metadata...")
+        logger.debug(f'Full path: {book_path}')
+        # ipdb.set_trace()
+    logger.info(f"Number of ebook files found: {number_ebooks}")
