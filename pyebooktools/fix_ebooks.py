4c0b3a192335c32a9ec37958998e114d1cdb8d6e
Raul <rchfe23@gmail.com>
2021-04-27 14:27:04 -0400

    Add fix_ebooks

---
+++ pyebooktools/fix_ebooks.py
@@ -0,0 +1,11 @@
+"""Tries to fix corrupted ebook files.
+
+For the moment, only PDF files are supported.
+"""
+from pyebooktools.utils.logutils import init_log
+
+logger = init_log(__name__, __file__)
+
+
+def fix(input_data, **kwargs):
+    logger.info('test')
