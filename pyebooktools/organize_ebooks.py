3831f94713cfc446e5d35484297f5d2756bd4ded
Raul <rchfe23@gmail.com>
2021-04-27 00:53:42 -0400

    organize: add description

---
+++ pyebooktools/organize_ebooks.py
@@ -0,0 +1,21 @@
+"""Automatically organizes folders with potentially huge amounts of unorganized
+ebooks.
+
+This is done by renaming the files with proper names and moving them to other
+folders.
+
+This is a Python port of `organize-ebooks.sh`_ from `ebook-tools`_ written in
+Shell by `na--`_.
+
+References
+----------
+* `ebook-tools`_
+
+.. TODO: add description to reference (and other places too)
+.. URLs
+
+.. external links
+.. _ebook-tools: https://github.com/na--/ebook-tools
+.. _na--: https://github.com/na--
+.. _organize-ebooks.sh: https://github.com/na--/ebook-tools/blob/master/organize-ebooks.sh
+"""
