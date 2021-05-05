283fd1847b7684a3011e25673400b68bfa28582b
Raul <rchfe23@gmail.com>
2021-05-05 00:35:51 -0400

    organize: log level to debug and add get_parts_from_path()

--- pyebooktools/organize_ebooks.py
+++ pyebooktools/organize_ebooks.py
@@ -37,24 +37,35 @@ from pyebooktools.utils.logutils import init_log
 logger = init_log(__name__, __file__)
 
 
+def get_parts_from_path(path):
+    path = Path(path)
+    return f'{path.anchor}'.join(path.parts[-2:])
+
+
 def fail_file(old_path, reason, new_path=None):
     # More info about printing in terminal with color:
     # https://stackoverflow.com/a/21786287
+    old_path = get_parts_from_path(old_path)
     logger.info(f'{RED}ERR{NC}\t: {old_path}')
     second_line = f'REASON\t: {reason}'
     if new_path:
+        new_path = get_parts_from_path(new_path)
         logger.info(second_line)
         logger.info(f'TO\t: {new_path}\n')
     else:
         logger.info(second_line + '\n')
 
 
-def ok_file(file_path, reason):
-    logger.info(f'{GREEN}OK{NC}\t: {file_path}\nTO\t: {reason}\n')
+def ok_file(old_path, new_path):
+    old_path = get_parts_from_path(old_path)
+    new_path = get_parts_from_path(new_path)
+    logger.info(f'{GREEN}OK{NC}\t: {old_path}\nTO\t: {new_path}\n')
 
 
 def skip_file(old_path, new_path):
     # TODO: https://bit.ly/2rf38f5
+    old_path = get_parts_from_path(old_path)
+    new_path = get_parts_from_path(new_path)
     logger.info(f'{BOLD}SKIP{NC}\t: {old_path}')
     logger.info(f'REASON\t: {new_path}\n')
 
@@ -96,7 +107,7 @@ class OrganizeEbooks:
         self.without_isbn_ignore = default_cfg.without_isbn_ignore
 
     def _is_pamphlet(self, file_path):
-        logger.info(f"Checking whether '{file_path}' looks like a pamphlet...")
+        logger.debug(f"Checking whether '{file_path}' looks like a pamphlet...")
         # TODO: check that it does the same as to_lower() @ https://bit.ly/2w0O5LN
         lowercase_name = os.path.basename(file_path).lower()
         # TODO: check that it does the same as
@@ -113,11 +124,11 @@ class OrganizeEbooks:
             for i, match in enumerate(matches):
                 parts.append(match.group())
             matches = ';'.join(parts)
-            logger.info('Parts of the filename match the pamphlet include '
-                        f'regex: [{matches}]')
+            logger.debug('Parts of the filename match the pamphlet include '
+                         f'regex: [{matches}]')
             return True
-        logger.info('The file does not match the pamphlet include regex, '
-                    'continuing...')
+        logger.debug('The file does not match the pamphlet include regex, '
+                     'continuing...')
         # TODO: check that it does the same as
         # `if [[ "$lowercase_name" =~ $PAMPHLET_EXCLUDED_FILES ]]; then`
         # ref.: https://bit.ly/2KscBZj
@@ -131,46 +142,46 @@ class OrganizeEbooks:
             for i, match in enumerate(matches):
                 parts.append(match.group())
             matches = ';'.join(parts)
-            logger.info('Parts of the filename match the pamphlet exclude '
-                        f'regex: [{matches}]')
+            logger.debug('Parts of the filename match the pamphlet exclude '
+                         f'regex: [{matches}]')
             # TODO: [ERROR] they are returning 1, but it should be returning 0
             # because the file is considered as a pamphlet
             return True
-        logger.info('The file does not match the pamphlet exclude regex, continuing...')
+        logger.debg('The file does not match the pamphlet exclude regex, continuing...')
         mime_type = get_mime_type(file_path)
         file_size_KiB = get_file_size(file_path, unit='KiB')
         if file_size_KiB is None:
             logger.error(f'Could not get the file size (KiB) for {file_path}')
             return None
         if mime_type == 'application/pdf':
-            logger.info('The file looks like a pdf, checking if the number of '
-                        f'pages is larger than {self.pamphlet_max_pdf_pages} ...')
+            logger.debug('The file looks like a pdf, checking if the number of '
+                         f'pages is larger than {self.pamphlet_max_pdf_pages} ...')
             result = get_pages_in_pdf(file_path)
             pages = result.stdout
             if pages is None:
                 logger.error(f'Could not get the number of pages for {file_path}')
                 return None
             elif pages > self.pamphlet_max_pdf_pages:
-                logger.info(f'The file has {pages} pages, too many for a pamphlet')
+                logger.debug(f'The file has {pages} pages, too many for a pamphlet')
                 return False
             else:
-                logger.info(f'The file has only {pages} pages, looks like a '
+                logger.debug(f'The file has only {pages} pages, looks like a '
                             'pamphlet')
                 return True
         elif file_size_KiB < self.pamphlet_max_filesize_kib:
-            logger.info(f"The file has a type '{mime_type}' and a small size "
-                        f'({file_size_KiB} KiB), looks like a pamphlet')
+            logger.debug(f"The file has a type '{mime_type}' and a small size "
+                         f'({file_size_KiB} KiB), looks like a pamphlet')
             return True
         else:
-            logger.info(f"The file has a type '{mime_type}' and a large size "
-                        '({file_size_KiB} KB), does NOT look like a pamphlet')
+            logger.debug(f"The file has a type '{mime_type}' and a large size "
+                         '({file_size_KiB} KB), does NOT look like a pamphlet')
             return False
 
     def _organize_by_filename_and_meta(self, old_path, prev_reason):
         # TODO: important, return nothing?
         prev_reason = f'{prev_reason}; '
-        logger.info(f"Organizing '{old_path}' by non-ISBN metadata and "
-                    "filename...")
+        logger.debug(f"Organizing '{old_path}' by non-ISBN metadata and "
+                     "filename...")
         # TODO: check that it does the same as to_lower() @ https://bit.ly/2w0O5LN
         lowercase_name = os.path.basename(old_path).lower()
         # TODO: check that it does the same as
@@ -213,7 +224,7 @@ class OrganizeEbooks:
         else:
             logger.debug(f"Couldn't determine if file '{old_path}' is a pamphlet")
         if not self.output_folder_uncertain:
-            logger.info('No uncertain folder specified, skipping...')
+            logger.debug('No uncertain folder specified, skipping...')
             skip_file(old_path, 'No uncertain folder specified')
             return
         result = get_ebook_metadata(old_path)
@@ -250,7 +261,7 @@ class OrganizeEbooks:
                     f.write(f'\nISBN                : {isbn}')
             else:
                 logger.debug(f'No isbn found for file {old_path}')
-            logger.info(f"Organizing '{old_path}' (with '{tmpmfile}')...")
+            logger.debug(f"Organizing '{old_path}' (with '{tmpmfile}')...")
             new_path = move_or_link_ebook_file_and_metadata(
                 new_folder=self.output_folder_uncertain,
                 current_ebook_path=old_path,
@@ -327,8 +338,8 @@ class OrganizeEbooks:
         isbn_sources = self.isbn_metadata_fetch_order
         for isbn in isbns.split(self.isbn_ret_separator):
             tmp_file = tempfile.mkstemp(suffix='.txt')[1]
-            logger.info(f"Trying to fetch metadata for ISBN '{isbn}' into temp file "
-                        f"'{tmp_file}'...")
+            logger.debug(f"Trying to fetch metadata for ISBN '{isbn}' into temp file "
+                         f"'{tmp_file}'...")
 
             # IMPORTANT: as soon as we find metadata from one source, we return
             for isbn_source in isbn_sources:
@@ -339,7 +350,7 @@ class OrganizeEbooks:
                 # e.g. WorldCat xISBN --> "WorldCat xISBN"
                 if ' ' in isbn_source:
                     isbn_source = f'"{isbn_source}"'
-                logger.info(f"Fetching metadata from '{isbn_source}' sources...")
+                logger.debug(f"Fetching metadata from '{isbn_source}' sources...")
                 options = f'--verbose --isbn={isbn}'
                 result = fetch_metadata(isbn_source, options)
                 metadata = result.stdout
@@ -352,7 +363,7 @@ class OrganizeEbooks:
                     # we are executing the rest of the code here once fetch_metadata()
                     # is done, ref.: https://bit.ly/2vV9MfU
                     time.sleep(0.1)
-                    logger.info('Successfully fetched metadata')
+                    logger.debug('Successfully fetched metadata')
                     logger.debug(f'Fetched metadata:{metadata}')
 
                     logger.debug('Adding additional metadata to the end of the metadata '
@@ -377,18 +388,18 @@ class OrganizeEbooks:
                     # move_or_link_ebook_file_and_metadata()
                     return
 
-            logger.info(f'Removing temp file {tmp_file}...')
+            logger.debug(f'Removing temp file {tmp_file}...')
             remove_file(tmp_file)
 
         if self.organize_without_isbn:
-            logger.info('Could not organize via the found ISBNs, organizing by '
-                        'filename and metadata instead...')
+            logger.debug('Could not organize via the found ISBNs, organizing by '
+                         'filename and metadata instead...')
             self._organize_by_filename_and_meta(
                 old_path=file_path,
                 prev_reason=f'Could not fetch metadata for ISBNs {isbns}')
         else:
-            logger.info('Organization by filename and metadata is not turned on, '
-                        'giving up...')
+            logger.debug('Organization by filename and metadata is not turned on, '
+                         'giving up...')
             skip_file(file_path, f'Could not fetch metadata for ISBNs {isbns}; '
                                  f'Non-ISBN organization disabled')
 
@@ -418,44 +429,44 @@ class OrganizeEbooks:
                         f.write(metadata)
                 fail_file(file_path, f'File is corrupt: {file_err}', new_path)
             else:
-                logger.info('Output folder for corrupt files is not set, doing '
-                            'nothing')
+                logger.debug('Output folder for corrupt files is not set, doing '
+                             'nothing')
                 fail_file(file_path, f'File is corrupt: {file_err}')
         elif self.corruption_check_only:
-            logger.info('We are only checking for corruption, do not continue '
-                        'organising...')
+            logger.debug('We are only checking for corruption, do not continue '
+                         'organising...')
             skip_file(file_path, 'File appears OK')
         else:
-            logger.info('File passed the corruption test, looking for ISBNs...')
+            logger.debug('File passed the corruption test, looking for ISBNs...')
             isbns = search_file_for_isbns(file_path, **self.__dict__)
             if isbns:
-                logger.info(f"Organizing '{file_path}' by ISBNs\n{isbns}")
+                logger.debug(f"Organizing '{file_path}' by ISBNs\n{isbns}")
                 self._organize_by_isbns(file_path, isbns)
             elif self.organize_without_isbn:
-                logger.info(f"No ISBNs found for '{file_path}', organizing by "
-                            'filename and metadata...')
+                logger.debug(f"No ISBNs found for '{file_path}', organizing by "
+                             'filename and metadata...')
                 self._organize_by_filename_and_meta(
                     old_path=file_path, prev_reason='No ISBNs found')
             else:
                 skip_file(file_path,
                           'No ISBNs found; Non-ISBN organization disabled')
-        logger.info('=====================================================')
+        logger.debug('=====================================================')
 
     def organize(self, folder_to_organize, **kwargs):
         # TODO: add debug message about update attributes
         self.__dict__.update(kwargs)
         self.folder_to_organize = folder_to_organize
         files = []
-        logger.info(f"Recursively scanning '{folder_to_organize}' for files...")
+        logger.debug(f"Recursively scanning '{folder_to_organize}' for files...")
         for fp in Path(folder_to_organize).rglob('*'):
             # Ignore directory and hidden files
             if Path.is_file(fp) and not fp.name.startswith('.'):
                 logger.debug(f"{fp.name}")
                 files.append((fp))
         # TODO: important sort within glob?
-        logger.info("Files sorted {}".format("in desc" if self.reverse else "in asc"))
+        logger.debug("Files sorted {}".format("in desc" if self.reverse else "in asc"))
         files.sort(key=lambda x: x.name, reverse=self.reverse)
-        logger.info('=====================================================')
+        logger.debug('=====================================================')
         for fp in files:
             self._organize_file(file_path=fp)
         return 0
