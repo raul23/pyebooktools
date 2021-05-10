dee69321518a357e0d50721e78d79ed76892e60b
Raul <rchfe23@gmail.com>
2021-05-09 20:21:31 -0400

    fix: add _fix_file()

--- pyebooktools/fix_ebooks.py
+++ pyebooktools/fix_ebooks.py
@@ -21,7 +21,7 @@ from pathlib import Path
 
 from pyebooktools.configs import default_config as default_cfg
 from pyebooktools.lib import (color_msg as c, check_input_data,
-                              get_parts_from_path)
+                              fix_file_for_corruption, get_parts_from_path)
 from pyebooktools.utils.logutils import init_log
 
 logger = init_log(__name__, __file__)
@@ -31,6 +31,14 @@ class FixEbooks:
     def __init__(self):
         self.input_data = None
         self.output_folder = default_cfg.output_folder
+        self.output_folder_corrupt = default_cfg.output_folder_corrupt
+        self.corruption_check_only = default_cfg.corruption_check_only
+        self.dry_run = default_cfg.dry_run
+        self.reverse = default_cfg.reverse
+        self.symlink_only = default_cfg.symlink_only
+
+    def _fix_file(self, file_path):
+        file_err = fix_file_for_corruption(file_path, self.output_folder)
 
     def fix(self, input_data, **kwargs):
         # TODO: add debug message about update attributes
@@ -39,13 +47,24 @@ class FixEbooks:
         if check_input_data(input_data):
             return 0
         found_pdf = False
+        files = []
         # NOTE: only PDF files supported
-        for fp in input_data.rglob('*.pdf'):
-            found_pdf = True
-            print(get_parts_from_path(fp))
+        # TODO: important, use get_mime_type?
+        if input_data.is_file() and input_data.suffix == '.pdf':
+            files = [input_data]
+        else:
+            # TODO: important, use get_mime_type for each file found?
+            for fp in input_data.rglob('*.pdf'):
+                found_pdf = True
+                # logger.debug(get_parts_from_path(fp))
+                files.append(fp)
+            # TODO: important sort within glob?
+            logger.debug("Files sorted {}".format("in desc" if self.reverse else "in asc"))
+            files.sort(key=lambda x: x.name, reverse=self.reverse)
+        for fp in files:
+            self._fix_file(fp)
         if not found_pdf:
-            logger.warning(f"{c('No PDF files found:')} "
-                           f"{input_data}")
+            logger.warning(f"{c('No PDF files found:')} {input_data}")
             logger.warning(f"{c('Only PDF files are supported!', bold=True)}")
         return 0
 
