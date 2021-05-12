8183082010416dec0520575c72e2deabfabeabeb
Raul <rchfe23@gmail.com>
2021-05-12 00:54:43 -0400

    fix: process file error message

--- pyebooktools/fix_ebooks.py
+++ pyebooktools/fix_ebooks.py
@@ -20,8 +20,9 @@ References
 from pathlib import Path
 
 from pyebooktools.configs import default_config as default_cfg
-from pyebooktools.lib import (color_msg as c, check_input_data,
-                              fix_file_for_corruption, get_parts_from_path as g)
+from pyebooktools.lib import (color_msg as c, check_input_data, fail_file,
+                              fix_file_for_corruption, get_parts_from_path as g,
+                              move_or_link_file, remove_file, unique_filename)
 from pyebooktools.utils.logutils import init_log
 
 logger = init_log(__name__, __file__)
@@ -36,19 +37,37 @@ class FixEbooks:
         self.dry_run = default_cfg.dry_run
         self.reverse = default_cfg.reverse
         self.symlink_only = default_cfg.symlink_only
+        self.output_metadata_extension = default_cfg.output_metadata_extension
 
     def _fix_file(self, file_path):
-        file_err = fix_file_for_corruption(file_path, **self.__dict__)
+        file_err, output_tmp_file = fix_file_for_corruption(file_path)
         if file_err:
-            logger.debug(f"File '{g(file_path)}' couldn't be "
-                         f"fixed!\n{file_err}")
+            logger.debug(f"File couldn't be fixed: {g(file_path)}"
+                         f"\n{file_err}")
             if self.output_folder_corrupt:
-                pass
+                new_path = unique_filename(self.output_folder_corrupt,
+                                           file_path.name)
+                move_or_link_file(file_path, new_path, self.dry_run,
+                                  self.symlink_only)
+                # TODO: do we add the meta extension directly to new_path?
+                new_metadata_path = f'{new_path}.{self.output_metadata_extension}'
+                logger.debug(f'Saving original filename to {new_metadata_path}...')
+                if not self.dry_run:
+                    file_err_tabs = file_err.replace('\n\t', '\n\t\t\t')
+                    metadata = f'Reason for not fixing:\t{file_err_tabs}\n' \
+                               f'Old file path:\t{file_path}'
+                    with open(new_metadata_path, 'w') as f:
+                        f.write(metadata)
+                fail_file(file_path, f"File couldn't be fixed: {file_err}",
+                          new_path)
             else:
                 msg = c('Output folder for corrupt files is not set, doing nothing')
                 logger.warning(f"{msg}")
         else:
             logger.debug('File was successfully fixed')
+        # Remove temporary file
+        if output_tmp_file:
+            remove_file(output_tmp_file)
         logger.debug('=====================================================')
 
     def _get_files(self):
@@ -73,7 +92,7 @@ class FixEbooks:
         self.__dict__.update(kwargs)
         self.input_data = input_data
         if check_input_data(input_data):
-            # TODO: 0 or 1
+            # TODO: 0 or 1?
             return 0
         files = self._get_files()
         logger.debug('=====================================================')
